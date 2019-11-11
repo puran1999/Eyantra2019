df99512e8d294a4189a668cd6e3c6d4a013ec8d0
Puran Singh <puransinghcn15@gmail.com>
2019-11-12 00:04:02 +0530

    code of Task 1.2
    copy into codes directory

--- main.py
+++ main.py
@@ -36,57 +36,35 @@ generated_folder_path = os.path.abspath(os.path.join('..', 'Generated'))
 def process(ip_image):
     ###########################
     ## Your Code goes here
-    
+
     pic = cv2.cvtColor(ip_image, cv2.COLOR_BGR2HSV)
-    
-    LLG = (55, 250, 250)#GREEN HSV=(60,255,255)
-    ULG = (65, 255, 255)
-    LLR = ( 0, 250, 250)#RED HSV=(0,255,255)
-    ULR = ( 0, 255, 255)
-    LLW = ( 0,   0, 250)#WHITE HSV=(0,0,255)
+    LLW = ( 0,   0, 245)#WHITE HSV=(0,0,255)
     ULW = ( 0,   0, 255)
-    
-    maskG = cv2.inRange(pic, LLG, ULG)
-    maskR = cv2.inRange(pic, LLR, ULR)
     maskW = cv2.inRange(pic, LLW, ULW)
-
-    contourG,h=cv2.findContours(maskG,cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    contourR,h=cv2.findContours(maskR,cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     contourW,h=cv2.findContours(maskW,cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    
-    cG, rG = cv2.minEnclosingCircle(contourG[0])
-    cR, rR = cv2.minEnclosingCircle(contourR[0])
-    cW, rW = cv2.minEnclosingCircle(contourW[0])
 
-    mG = (cG[0]-cW[0])/(cG[1]-cW[1])
-    mR = (cR[0]-cW[0])/(cR[1]-cW[1])
-    aG = math.degrees(math.atan(mG))
-    aR = math.degrees(math.atan(mR))
-    if cG[1]>cW[1]:
-        aG=180+aG
-    if cR[1]>cW[1]:
-        aR=180+aR
-    if aG > 180:
-        aG=aG-360
-    if aR > 180:
-        aR=aR-360
-    #print(aG, aR)
-    angle = -1
-    if aG > aR:
-        angle = aG-aR
-    if aR > aG:
-        angle = aR-aG
-    angle = abs(angle)
-    if angle > 180:
-        angle = 360-angle
-    angle = round(angle,2)
-    print(angle)
-    
+    detected = []
+    for cont in contourW:
+        area = cv2.contourArea(cont)
+        equi_diameter = np.sqrt(4*area/np.pi)
+        center, radius = cv2.minEnclosingCircle(cont)
+        ratio = (2*radius-equi_diameter)/equi_diameter
+        #print(ratio)
+        if ratio>0.5:
+            detected = cont
+        
+    ## placeholder image
+    sector_image = np.ones(ip_image.shape[:2],np.uint8)*255
+    ## check value is white or not
+    # print(sector_image[0,0])    
+
+    sector_image = cv2.drawContours(sector_image, [detected], 0, 0, -1)
+
     ## Your Code goes here
     ###########################
-    #cv2.imshow("window", ip_image)
-    #cv2.waitKey(0);
-    return angle
+    cv2.imshow("window", sector_image)
+    cv2.waitKey(0);
+    return sector_image
 
 
 
@@ -103,7 +81,6 @@ def main():
     ## variable declarations
     ################################################################
     i = 1
-    line = []
     ## Reading 1 image at a time from the Images folder
     for image_name in os.listdir(images_folder_path):
         ## verifying name of image
@@ -113,20 +90,10 @@ def main():
         ## verifying image has content
         print(ip_image.shape)
         ## passing read in image to process function
-        A = process(ip_image)
-        ## saving the output in  a list variable
-        line.append([str(i), image_name , str(A)])
-        ## incrementing counter variable
+        sector_image = process(ip_image)
+        ## saving the output in  an image of said name in the Generated folder
+        cv2.imwrite(generated_folder_path+"/"+"image_"+str(i)+"_fill_in.png", sector_image)
         i+=1
-    ## verifying all data
-    print(line)
-    ## writing to angles.csv in Generated folder without spaces
-    with open(generated_folder_path+"/"+'angles.csv', 'w', newline='') as writeFile:
-        writer = csv.writer(writeFile)
-        writer.writerows(line)
-    ## closing csv file    
-    writeFile.close()
-
 
 
     
