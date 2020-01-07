922dd4a80752076e39082d09f56cc0bc85903c8e
jagmeetz <57046437+jagmeetz@users.noreply.github.com>
2020-01-07 16:25:09 +0530

    Code improved
    HSV values improved and faulty contour reported

--- task3#sb/3.2. Test Setup/test_task3.py
+++ task3#sb/3.2. Test Setup/test_task3.py
@@ -15,6 +15,7 @@ import math
 import csv
 import copy
 
+
 def find_correct_contours(contour):
     detected = []
     for cont in contour:
@@ -23,22 +24,34 @@ def find_correct_contours(contour):
         center, radius = cv2.minEnclosingCircle(cont)
         ratio = (2*radius-equi_diameter)/equi_diameter
         #print(ratio)
-        if 0.06 < ratio < 0.2:
+        if 0.06 < ratio < 0.18:
             detected.append(cont)
             #print("detected")
     return detected
 
 def enquire(contourG, contourR, contourW):
-    if not contourG or not contourR or not contourW:
-        return 0
-    else:
-        return 1
+    res = 1
+    if not contourG:
+        print("Green not found")
+        res = 0
+    if not contourR:
+        print("Red not found")
+        res = 0
+    if not contourW:
+        print("White not found")
+        res = 0
+    return res
+    
 
 def get_centre(contour):
     M = cv2.moments(contour)
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
     return [cX, cY]
+
+
+
+
 
 ############################################
 ## Build your algorithm in this function
@@ -53,17 +66,17 @@ def process(ip_image):
     angle = -1
     pic = cv2.cvtColor(ip_image, cv2.COLOR_BGR2HSV)
     
-    LLG = (37, 62, 0)#GREEN HSV=(60,255,255)
+    LLG = (25, 62, 110)#GREEN HSV=(60,255,255)
     ULG = (76, 255, 255)
-    LLR = ( 160, 160, 0)#RED HSV=(0,255,255)
+    LLR = ( 164, 150, 150)#RED HSV=(0,255,255)
     ULR = ( 180, 255, 255)
-    LLW = ( 130, 8, 198)#WHITE HSV=(0,0,255)
-    ULW = ( 180, 74, 255)
+    LLW = ( 0, 0, 0)#WHITE HSV=(0,0,255)
+    ULW = ( 180, 41, 255)
     
     maskG = cv2.inRange(pic, LLG, ULG)
     maskR = cv2.inRange(pic, LLR, ULR)
     maskW = cv2.inRange(pic, LLW, ULW)
-
+    
     contourG,h=cv2.findContours(maskG, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     contourR,h=cv2.findContours(maskR, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     contourW,h=cv2.findContours(maskW, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
@@ -73,8 +86,9 @@ def process(ip_image):
     contourR = find_correct_contours(contourR)
 
     cv2.drawContours(ip_image, contourG, -1, (255, 0, 0), 2)
-    cv2.drawContours(ip_image, contourR, -1, (255, 0, 0), 2)
-     
+    cv2.drawContours(ip_image, contourR, -1, (255, 255, 0), 2)
+    cv2.drawContours(ip_image, contourW, -1, (0, 255, 255), 2)
+
 
     if enquire(contourG, contourR, contourW):
         cG = get_centre(contourG[0])
@@ -116,10 +130,10 @@ def process(ip_image):
         ip_image = cv2.putText(ip_image, "Angle: " + str(angle), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255))
     else:
         print("One of three contour not found")
- 
+    cv2.imshow("Result", ip_image)
     op_image = ip_image
     return op_image
-
+    
     
 ####################################################################
 ## The main program which provides read in input of one image at a
@@ -133,7 +147,7 @@ def main():
     ################################################################
     i = 1
     ## reading in video 
-    cap = cv2.VideoCapture(0) #if you have a webcam on your system, then change 0 to 1
+    cap = cv2.VideoCapture(1) #if you have a webcam on your system, then change 0 to 1
     ## getting the frames per second value of input video
     fps = cap.get(cv2.CAP_PROP_FPS)
     ## setting the video counter to frame sequence
@@ -146,11 +160,11 @@ def main():
     while(ret):
         ret, frame = cap.read()
         ## display to see if the frame is correct
-        cv2.imshow("window", frame)
+        #cv2.imshow("window", frame)
         cv2.waitKey(int(1000/fps));
         ## calling the algorithm function
         op_image = process(frame)
-        cv2.imwrite("SB#9999_task3I.jpg",op_image)
+        cv2.imwrite("SB#4277_task3I.jpg",op_image)
 
 
     
